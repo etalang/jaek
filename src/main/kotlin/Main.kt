3e24f8d61b7c72936f259f7b9af2f5f84656829a
tnv5 <tnv5@cornell.edu>
2023-02-02 15:15:00 -0500

    Note: Build NOT WORKING. Added some logic for creating files and lexing.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 
+import JFlexLexer.LexicalError
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
@@ -8,7 +9,7 @@ import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
 import java.io.File
 import kotlin.io.path.Path
-//import lexer somehow still - need to figure out file structure for the package
+import JFlexLexer as JFLexLexer
 
 class Etac : CliktCommand(printHelpOnEmptyArgs = true) {
     // collect input options, specify help message
@@ -25,7 +26,24 @@ class Etac : CliktCommand(printHelpOnEmptyArgs = true) {
         print(lexFiles)
         lexFiles.forEach{
             if(it.extension == "eta"){
-                //We should lex the file in this case
+                //We should lex the file in this case and output it to a file
+
+                //Create the new lexer
+                val lex = JFLexLexer(it.bufferedReader())
+                //Create the new file
+                val lexedFileName = it.nameWithoutExtension + ".lexed"
+                val lexedFile = File(diagnosticAbsPath.toString(),lexedFileName)
+                lexedFile.createNewFile()
+
+                while (true) {
+                    try {
+                        val t: JFLexLexer.Token = lex.nextToken() ?: break
+                        lexedFile.appendText(t.toString() + "\n")
+                    } catch (e: LexicalError) {
+                        lexedFile.appendText(e.lineNum.toString() + " " + e.col.toString() + " " + e.msg.toString() + "\n")
+                        break
+                    }
+                }
 
             } else {
                 echo("The file " + it + " is not an eta file. Skipping.")
