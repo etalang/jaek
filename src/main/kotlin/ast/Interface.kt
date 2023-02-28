591067d86e3ec2d21dc3929c7e90840ae484316c
tnv5 <tnv5@cornell.edu>
2023-02-28 12:17:46 -0500

    required eti files to have bodies and allow imports

--- src/main/kotlin/ast/Interface.kt
+++ src/main/kotlin/ast/Interface.kt
@@ -2,9 +2,14 @@ package ast
 
 import edu.cornell.cs.cs4120.util.SExpPrinter
 
-class Interface(val methodHeaders : ArrayList<Method>) : Eta() {
+class Interface(val imports: ArrayList<Use>, val methodHeaders : ArrayList<Method>) : Eta() {
     override fun write(printer: SExpPrinter) {
         printer.startList()
+        if (imports.size > 0) {
+            printer.startList()
+            imports.forEach { use -> use.write(printer) }
+            printer.endList()
+        }
         printer.startList()
         methodHeaders.forEach {mh -> mh.write(printer)}
         printer.endList()
