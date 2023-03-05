d3c5c30862270e29ef1cdd27a209054e8cb46745
noschiff <noah@wow.com>
2023-03-05 14:54:10 -0500

    rename xic to etac

--- src/main/java/edu/cornell/cs/cs4120/xic/ir/IRExpr.java
+++
@@ -1,7 +0,0 @@
-package edu.cornell.cs.cs4120.xic.ir;
-
-public interface IRExpr extends IRNode {
-    boolean isConstant();
-
-    long constant();
-}
--- src/main/java/edu/cornell/cs/cs4120/xic/ir/IRStmt.java
+++
@@ -1,4 +0,0 @@
-package edu.cornell.cs.cs4120.xic.ir;
-
-/** An intermediate representation for statements */
-public abstract class IRStmt extends IRNode_c {}
