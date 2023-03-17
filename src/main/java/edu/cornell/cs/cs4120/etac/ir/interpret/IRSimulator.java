caf7a6171dcad3041625ae92ec497c49ae6ffc46
Kate Meuse <katemeuse@gmail.com>
2023-03-17 01:30:22 -0400

    multiply default heap size by 10

--- src/main/java/edu/cornell/cs/cs4120/etac/ir/interpret/IRSimulator.java
+++ src/main/java/edu/cornell/cs/cs4120/etac/ir/interpret/IRSimulator.java
@@ -58,7 +58,7 @@ public class IRSimulator {
 
     protected static int debugLevel = 0;
 
-    public static final int DEFAULT_HEAP_SIZE = 10240;
+    public static final int DEFAULT_HEAP_SIZE = 102400;
 
     /**
      * Construct an IR interpreter with a default heap size
