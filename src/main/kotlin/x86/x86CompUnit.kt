3042f45e4f54537d6b67a95462fd3bf2179f34c1
Bryan Lu <bryan.lu3@gmail.com>
2023-03-29 11:56:51 -0400

    fix runtime, add .s files to gitignore

--- src/main/kotlin/x86/x86CompUnit.kt
+++ src/main/kotlin/x86/x86CompUnit.kt
@@ -5,11 +5,11 @@ import ir.IRData
 class x86CompUnit(val name : String, val functions: List<x86FuncDecl>, val globals: List<x86Data>) {
     override fun toString(): String {
         var fullText = mutableListOf<String>(
-            ".file $name",
+            ".file \"$name.eta\"",
             ".intel_syntax noprefix",
-            ""
         )
         if (globals.isNotEmpty()) {
+            fullText.add("")
             fullText.add(".data")
             for (data in globals) {
                 fullText.add(data.toString())
