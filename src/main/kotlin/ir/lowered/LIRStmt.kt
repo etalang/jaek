583d58fb2d128a645276d1b975212bab674ad2d6
Bryan Lu <bryan.lu3@gmail.com>
2023-04-11 15:54:07 -0400

    finish call

--- src/main/kotlin/ir/lowered/LIRStmt.kt
+++ src/main/kotlin/ir/lowered/LIRStmt.kt
@@ -1,5 +1,6 @@
 package ir.lowered
 
+//import assembly.Tile
 import assembly.tile.BuiltTile
 import assembly.x86.*
 import edu.cornell.cs.cs4120.etac.ir.IRCJump as JIRCJump
@@ -12,7 +13,7 @@ import edu.cornell.cs.cs4120.etac.ir.IRStmt as JIRStmt
 
 /** IRStmt represents a statement **/
 sealed class LIRStmt<TileType> : LIRNode.TileableNode<TileType>() where TileType : BuiltTile{
-    override abstract val java: JIRStmt;
+    abstract override val java: JIRStmt;
 
     sealed class FlatStmt<TileType> : LIRStmt<TileType>() where TileType : BuiltTile
     sealed class EndBlock<TileType> : FlatStmt<TileType>() where TileType : BuiltTile
@@ -35,7 +36,7 @@ sealed class LIRStmt<TileType> : LIRNode.TileableNode<TileType>() where TileType
                                 ), 1
                             )
                         } else {
-                            val destTile = expr.optimalTile()
+                            val destTile = dest.optimalTile()
                             BuiltTile.RegularTile(
                                 srcTile.instructions.plus(destTile.instructions).plus(
                                     Instruction.MOV(
@@ -51,7 +52,7 @@ sealed class LIRStmt<TileType> : LIRNode.TileableNode<TileType>() where TileType
                         BuiltTile.RegularTile(
                             srcTile.instructions.plus(
                                 Instruction.MOV(
-                                    Destination.RegisterDest(Register.Abstract((dest).name)),
+                                    Destination.RegisterDest(Register.Abstract(dest.name)),
                                     Source.RegisterSrc(srcTile.outputRegister)
                                 )
                             ), 1
@@ -187,11 +188,121 @@ sealed class LIRStmt<TileType> : LIRNode.TileableNode<TileType>() where TileType
     class LIRCallStmt(val target: LIRExpr.LIRName, val n_returns: Long, val args: List<LIRExpr>) : FlatStmt<BuiltTile.RegularTile>() {
         override val java: JIRCallStmt = factory.IRCallStmt(target.java, n_returns, args.map { it.java })
 
-        override val defaultTile get() = TODO("Not yet implemented")
-
-        override fun findBestTile() {
-            TODO("Not yet implemented")
+        override val defaultTile : BuiltTile.RegularTile
+            get() {
+            val insns = mutableListOf<Instruction>()
+            val reglst = mutableListOf<Register>()
+            val argNumber = args.size
+            var argOffset = 0
+            for (arg in args) {
+                // ensures arguments are still evaluated left to right
+                val argTile = arg.optimalTile()
+                reglst.add(argTile.outputRegister)
+                insns.addAll(argTile.instructions)
+            }
+            if (n_returns >= 3) {
+                argOffset = 1
+                insns.add(
+                    Instruction.Arith.SUB(
+                        Destination.RegisterDest(Register.x86(Register.x86Name.RSP)),
+                        Source.ConstSrc(8L * (n_returns - 2L))
+                    ))
+                insns.add(
+                    Instruction.MOV(
+                        Destination.RegisterDest(Register.x86(Register.x86Name.RDI)),
+                        Source.RegisterSrc(Register.x86(Register.x86Name.RSP))
+                    )
+                )
+            }
+            if (argNumber > 6 - argOffset) {
+                for (i in args.size - 1 downTo 6 - argOffset) {
+                    insns.add(Instruction.PUSH(reglst[i]))
+                }
+            }
+            if (argNumber > 5 - argOffset) {
+                insns.add(
+                    Instruction.MOV(
+                        Destination.RegisterDest(Register.x86(Register.x86Name.R9)),
+                        Source.RegisterSrc(reglst[5 - argOffset])
+                    )
+                )
+            }
+            if (argNumber > 4 - argOffset) {
+                insns.add(
+                    Instruction.MOV(
+                        Destination.RegisterDest(Register.x86(Register.x86Name.R8)),
+                        Source.RegisterSrc(reglst[4 - argOffset])
+                    )
+                )
+            }
+            if (argNumber > 3 - argOffset) {
+                insns.add(
+                    Instruction.MOV(
+                        Destination.RegisterDest(Register.x86(Register.x86Name.RCX)),
+                        Source.RegisterSrc(reglst[3 - argOffset])
+                    )
+                )
+            }
+            if (argNumber > 2 - argOffset) {
+                insns.add(
+                    Instruction.MOV(
+                        Destination.RegisterDest(Register.x86(Register.x86Name.RDX)),
+                        Source.RegisterSrc(reglst[2 - argOffset])
+                    )
+                )
+            }
+            if (argNumber > 1 - argOffset) {
+                insns.add(
+                    Instruction.MOV(
+                        Destination.RegisterDest(Register.x86(Register.x86Name.RSI)),
+                        Source.RegisterSrc(reglst[1 - argOffset])
+                    )
+                )
+            }
+            if (n_returns < 3) {
+                insns.add(
+                    Instruction.MOV(
+                        Destination.RegisterDest(Register.x86(Register.x86Name.RDI)),
+                        Source.RegisterSrc(reglst.first())
+                    )
+                )
+            }
+//            insns.add(
+//                Instruction.Logic.AND(
+//                    Destination.RegisterDest(Register.x86(Register.x86Name.RSP)),
+//                    Source.ConstSrc(-16)
+//                ))
+            insns.add(Instruction.CALL(Label(target.l, false)))
+            if (argNumber > 6 - argOffset) {
+                insns.add(
+                    Instruction.Arith.ADD(
+                        Destination.RegisterDest(Register.x86(Register.x86Name.RSP)),
+                        Source.ConstSrc(8L * (argNumber - (6 - argOffset)))
+                    ))
+            }
+            insns.add(
+                Instruction.MOV(
+                    Destination.RegisterDest(Register.Abstract("_RV1")),
+                    Source.RegisterSrc(Register.x86(Register.x86Name.RAX))
+                )
+            )
+            if (n_returns >= 2) {
+                insns.add(
+                    Instruction.MOV(
+                        Destination.RegisterDest(Register.Abstract("_RV2")),
+                        Source.RegisterSrc(Register.x86(Register.x86Name.RDX))
+                    )
+                )
+            }
+            if (n_returns >= 3) {
+                for (i in 3 .. n_returns.toInt()) {
+                    insns.add(Instruction.POP(Register.Abstract("_RV$i")))
+                }
+            }
+            return BuiltTile.RegularTile(insns, args.size)
         }
+
+        override fun findBestTile() {}
     }
 
 }
