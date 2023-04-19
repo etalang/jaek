7ea91105851eb152e42056da6de960f33e7c6da9
noschiff <noah@wow.com>
2023-04-19 00:41:31 -0400

    document assembly instructions

--- src/main/kotlin/assembly/x86/Instruction.kt
+++ src/main/kotlin/assembly/x86/Instruction.kt
@@ -1,5 +1,12 @@
 package assembly.x86
 
+/**
+ * Flag Information:
+ * - ZF = 1 <=> result = 0
+ * - SF = 1 <=> result is (-)
+ * - OF = 1 <=> result overflowed
+ * - PF = 1 <=> sum of bits is even
+ */
 sealed class Instruction {
     abstract val written: Set<Register>
     abstract val read: Set<Register>
@@ -16,6 +23,13 @@ sealed class Instruction {
         }
     }
 
+    /**
+     * copies [src] to [dest]
+     *
+     * The source operand can be an immediate value, general-purpose register, segment register, or memory location; the
+     * destination register can be a general-purpose register, segment register, or memory location. Both operands must
+     * be the same size, which can be a byte, a word, a doubleword, or a quadword.
+     */
     data class MOV(val dest: Destination, val src: Source) : Instruction() {
         override val written: Set<Register> = dest.written
         override val read: Set<Register> = dest.read union src.read
@@ -29,25 +43,54 @@ sealed class Instruction {
         override val written: Set<Register> = dest.written
         override val read: Set<Register> = dest.read union src.read
 
+        /**
+         * [dest] := [dest] + [src]
+         *
+         * The ADD instruction performs integer addition. It evaluates the result for both signed and unsigned integer
+         * operands and sets the OF and CF flags to indicate a carry (overflow) in the signed or unsigned result,
+         * respectively. The SF flag indicates the sign of the signed result.
+         */
         class ADD(dest: Destination, src: Source) : Arith(dest, src) {
             override fun toString(): String {
                 return "add $dest, $src"
             }
         }
 
+        /**
+         * [dest] := [dest] - [src]
+         *
+         * The SUB instruction performs integer subtraction. It evaluates the result for both signed and unsigned
+         * integer operands and sets the OF and CF flags to indicate an overflow in the signed or unsigned result,
+         * respectively. The SF flag indicates the sign of the signed result.
+         */
         class SUB(dest: Destination, src: Source) : Arith(dest, src) {
             override fun toString(): String {
                 return "sub $dest, $src"
             }
         }
 
+        /**
+         * [dest] is multiplied by the [src]. The destination operand is a general-purpose register and the source
+         * operand is an immediate value, a general-purpose register, or a memory location. The intermediate product
+         * (twice the size of the input operand) is truncated and stored in the destination operand location.
+         *
+         * The CF and OF flags are set when the result must be truncated to fit in the destination operand size and
+         * cleared when the result fits exactly in the destination operand size. The SF, ZF, AF, and PF flags are
+         * undefined.
+         */
         class MUL(dest: Destination, src: Source) : Arith(dest, src) {
             override fun toString(): String {
                 return "imul $dest, $src"
             }
         }
 
-        class LEA(dest: Destination, src: Source) : Arith(dest, src)
+        /**
+         * Computes the effective address of the [src] and stores it [dest].
+         *
+         * The source operand is a memory address (offset part) specified with one of the processors addressing modes;
+         * the destination operand is a general-purpose register.
+         */
+        class LEA(dest: Destination, src: Source.MemorySrc) : Arith(dest, src)
 
     }
 
@@ -55,36 +98,73 @@ sealed class Instruction {
         override val written: Set<Register> = dest.written
         override val read: Set<Register> = dest.read union src.read
 
+        /**
+         * [dest] := [dest] AND [src]
+         *
+         * The OF and CF flags are cleared; the SF, ZF, and PF flags are set according to the result. The state of the
+         * AF flag is undefined.
+         */
         class AND(dest: Destination, src: Source) : Logic(dest, src) {
             override fun toString(): String {
                 return "and $dest, $src"
             }
         }
 
+        /**
+         * [dest] := [dest] OR [src]
+         *
+         * The OF and CF flags are cleared; the SF, ZF, and PF flags are set according to the result.
+         */
         class OR(dest: Destination, src: Source) : Logic(dest, src) {
             override fun toString(): String {
                 return "or $dest, $src"
             }
         }
 
+        /**
+         * [dest] := [dest] XOR [src]
+         *
+         * The OF and CF flags are cleared; the SF, ZF, and PF flags are set according to the result.
+         */
         class XOR(dest: Destination, src: Source) : Logic(dest, src) {
             override fun toString(): String {
                 return "xor $dest, $src"
             }
         }
 
+        /**
+         * The CF flag contains the value of the last bit shifted out of the destination operand; it is undefined for
+         * SHL and SHR instructions where the count is greater than or equal to the size (in bits) of the destination
+         * operand. The OF flag is affected only for 1-bit shifts; otherwise, it is undefined. The SF, ZF, and PF flags
+         * are set according to the result. If the count is 0, the flags are not affected. For a non-zero count, the AF
+         * flag is undefined.
+         */
         class SHL(dest: Destination, src: Source) : Logic(dest, src) {
             override fun toString(): String {
                 return "shl $dest, $src"
             }
         }
 
+        /**
+         * The CF flag contains the value of the last bit shifted out of the destination operand; it is undefined for
+         * SHL and SHR instructions where the count is greater than or equal to the size (in bits) of the destination
+         * operand. The OF flag is affected only for 1-bit shifts; otherwise, it is undefined. The SF, ZF, and PF flags
+         * are set according to the result. If the count is 0, the flags are not affected. For a non-zero count, the AF
+         * flag is undefined.
+         */
         class SHR(dest: Destination, src: Source) : Logic(dest, src) {
             override fun toString(): String {
                 return "shr $dest, $src"
             }
         }
 
+        /**
+         * The CF flag contains the value of the last bit shifted out of the destination operand; it is undefined for
+         * SHL and SHR instructions where the count is greater than or equal to the size (in bits) of the destination
+         * operand. The OF flag is affected only for 1-bit shifts; otherwise, it is undefined. The SF, ZF, and PF flags
+         * are set according to the result. If the count is 0, the flags are not affected. For a non-zero count, the AF
+         * flag is undefined.
+         */
         class SAR(dest: Destination, src: Source) : Logic(dest, src) {
             override fun toString(): String {
                 return "sar $dest, $src"
@@ -95,6 +175,11 @@ sealed class Instruction {
 
     // TODO: fix the typing on these instructions to be correct (see manual)
     // the arity is correct, but the types are hella wrong below:
+    /**
+     * temp := [reg1] − SignExtend([reg2]);
+     *
+     * The CF, OF, SF, ZF, AF, and PF flags are set according to the result.
+     */
     data class CMP(val reg1: Register, val reg2: Register) : Instruction() {
         override val written: Set<Register> = setOf()
         override val read: Set<Register> = setOf(reg1, reg2)
@@ -104,6 +189,12 @@ sealed class Instruction {
         }
     }
 
+    /**
+     * TEMP := [reg1] AND [reg2];
+     *
+     * SF := MSB(TEMP); IF TEMP = 0 THEN ZF := 1; ELSE ZF := 0; FI: PF := BitwiseXNOR(TEMP[0:7]); CF := 0; OF := 0; (*
+     * AF is undefined *)
+     */
     data class TEST(val reg1: Register, val reg2: Register) : Instruction() {
         override val written: Set<Register> = setOf()
         override val read: Set<Register> = setOf(reg1, reg2)
@@ -113,8 +204,11 @@ sealed class Instruction {
         }
     }
 
-    /** sets the low 8 bits of register with 0 or 1 if the corresponding jump would be taken
-     * @param register where size must be 8 */
+    /**
+     * sets the low 8 bits of register with 0 or 1 if the corresponding jump would be taken
+     *
+     * @param reg where size must be 8
+     */
     sealed class JumpSet(val reg: Register) : Instruction() {
         override val written: Set<Register> = setOf(reg)
         override val read: Set<Register> = setOf()
@@ -143,9 +237,7 @@ sealed class Instruction {
             }
         }
 
-        /**
-         * Specifically for unsigned jump JB (below)
-         */
+        /** Specifically for unsigned jump JB (below) */
         class SETB(reg: Register) : JumpSet(reg) {
             override fun toString(): String {
                 return "setb $reg"
@@ -170,55 +262,63 @@ sealed class Instruction {
         override val written: Set<Register> = setOf()
         override val read: Set<Register> = setOf()
 
+        /** Transfers program control to instruction at [loc] */
         class JMP(loc: Location) : Jump(loc) {
             override fun toString(): String {
                 return "jmp $loc"
             }
         }
 
+        /** Jump if **ZF = 1** to [loc] */
         class JE(loc: Location) : Jump(loc) {
             override fun toString(): String {
                 return "je $loc"
             }
         }
-        // TODO: implement toString for all other jumps
 
+        /** Jump if **ZF = 0** to [loc] */
         class JNE(loc: Location) : Jump(loc) {
             override fun toString(): String {
                 return "jne $loc"
             }
         }
 
+        /** Jump if **SF ≠ OF** to [loc] */
         class JL(loc: Location) : Jump(loc) {
             override fun toString(): String {
                 return "jl $loc"
             }
         }
 
+        /** Jump if **ZF=1 or SF ≠ OF** to [loc] */
         class JLE(loc: Location) : Jump(loc) {
             override fun toString(): String {
                 return "jle $loc"
             }
         }
 
+        /** Jump if **ZF = 0 and SF = OF** to [loc] */
         class JG(loc: Location) : Jump(loc) {
             override fun toString(): String {
                 return "jg $loc"
             }
         }
 
+        /** Jump if **SF = OF** to [loc] */
         class JGE(loc: Location) : Jump(loc) {
             override fun toString(): String {
                 return "jge $loc"
             }
         }
 
+        /** Jump if **ZF = 1** */
         class JZ(loc: Location) : Jump(loc) {
             override fun toString(): String {
                 return "jz $loc"
             }
         }
 
+        /** Jump if **ZF = 0** */
         class JNZ(loc: Location) : Jump(loc) {
             override fun toString(): String {
                 return "jnz $loc"
@@ -226,7 +326,7 @@ sealed class Instruction {
         }
     }
 
-    /** extends sign of argument in RAX into RDX. used in division */
+    /** RDX:RAX := sign-extend of RAX */
     class CQO : Instruction() {
         override val written: Set<Register> = setOf()
         override val read: Set<Register> = setOf()
@@ -236,6 +336,7 @@ sealed class Instruction {
         }
     }
 
+    /** Decrements the stack pointer and then stores [arg] on the top of the stack. */
     data class PUSH(val arg: Register) : Instruction() {
         override val written: Set<Register> = setOf()
         override val read: Set<Register> = setOf(arg)
@@ -245,6 +346,11 @@ sealed class Instruction {
         }
     }
 
+    /**
+     * Loads the value from the top of the stack to [dest] and then increments the stack pointer.
+     *
+     * TODO: The destination operand can be a general-purpose register, memory location, or segment register.
+     */
     data class POP(val dest: Register) : Instruction() {
         override val written: Set<Register> = setOf(dest)
         override val read: Set<Register> = setOf()
@@ -254,6 +360,12 @@ sealed class Instruction {
         }
     }
 
+    /**
+     * Saves procedure linking information on the stack and branches to the called procedure specified using the target
+     * operand. [label] specifies the address of the first instruction in the called procedure.
+     *
+     * TODO: The operand can be an immediate value, a general-purpose register, or a memory location.
+     */
     class CALL(val label: Label) : Instruction() {
         override val written: Set<Register> = setOf()
         override val read: Set<Register> = setOf()
@@ -263,6 +375,14 @@ sealed class Instruction {
         }
     }
 
+    /**
+     * Creates a stack frame for a procedure. [bytes] specifies the size of the dynamic storage in the stack frame (that
+     * is, the number of bytes of dynamically allocated on the stack for the procedure).
+     *
+     * TODO: The second operand (imm8) gives the lexical nesting level (0 to 31) of the procedure. The nesting level
+     *     (imm8 mod 32) and the OperandSize attribute determine the size in bytes of the storage space for frame
+     *     pointers.
+     */
     class ENTER(val bytes: Long) : Instruction() {
         override val written: Set<Register> = setOf()
         override val read: Set<Register> = setOf()
@@ -272,6 +392,13 @@ sealed class Instruction {
         }
     }
 
+    /**
+     * Releases the stack frame set up by an earlier ENTER instruction.
+     *
+     * Copies the frame pointer into the stack pointer register, which releases the stack space allocated to the
+     * stack frame. The old frame pointer (the frame pointer for the calling procedure that was saved by the ENTER
+     * instruction) is then popped from the stack into the EBP register, restoring the calling procedure’s stack frame.
+     */
     class LEAVE : Instruction() {
         override val written: Set<Register> = setOf()
         override val read: Set<Register> = setOf()
@@ -281,6 +408,10 @@ sealed class Instruction {
         }
     }
 
+    /**
+     * Transfers program control to a return address located on the top of the stack. The address is usually placed on
+     * the stack by a CALL instruction, and the return is made to the instruction that follows the CALL instruction.
+     */
     class RET : Instruction() {
         override val written: Set<Register> = setOf()
         override val read: Set<Register> = setOf()
@@ -290,6 +421,7 @@ sealed class Instruction {
         }
     }
 
+    /** Jaek */
     class NOP : Instruction() {
         override val written: Set<Register> = setOf()
         override val read: Set<Register> = setOf()
@@ -299,10 +431,10 @@ sealed class Instruction {
         }
     }
 
+    /** RDX:RAX := RAX * [factor]. */
     class IMULSingle(val factor: Register) : Instruction() {
         override val written: Set<Register> = setOf(
-            Register.x86(Register.x86Name.RAX),
-            Register.x86(Register.x86Name.RDX)
+            Register.x86(Register.x86Name.RAX), Register.x86(Register.x86Name.RDX)
         )
         override val read: Set<Register> = setOf(factor)
 
@@ -311,10 +443,10 @@ sealed class Instruction {
         }
     }
 
+    /** Unsigned divide RDX:RAX by [divisor], with result stored in RAX := Quotient, RDX := Remainder */
     class DIV(val divisor: Register) : Instruction() {
         override val written: Set<Register> = setOf(
-            Register.x86(Register.x86Name.RAX),
-            Register.x86(Register.x86Name.RDX)
+            Register.x86(Register.x86Name.RAX), Register.x86(Register.x86Name.RDX)
         )
         override val read: Set<Register> = setOf(divisor)
 
