97e26f47afbdb998ee5fb47cdadfcdc5c201c6f6
noschiff <noah@wow.com>
2023-05-17 19:04:00 -0400

    run more tests with timer

--- src/main/kotlin/optimize/IROptimizer.kt
+++ src/main/kotlin/optimize/IROptimizer.kt
@@ -8,6 +8,7 @@ import optimize.cfg.CFGDestroyer
 import optimize.dataflow.CondConstProp
 import optimize.dataflow.CopyProp
 import optimize.dataflow.DeadCodeRem
+import kotlin.system.measureTimeMillis
 
 class IROptimizer(val lir: LIRFuncDecl, optimize: Settings.Opt, val outputCFG: Settings.OutputCFG) {
     var cfg: CFG
@@ -17,33 +18,39 @@ class IROptimizer(val lir: LIRFuncDecl, optimize: Settings.Opt, val outputCFG: S
         cfg = builder.build()
         val funcFile = outputCFG.getOutFile(lir.name, "initial")
         funcFile?.writeText(cfg.graphViz())
+        var count = 0
 
-        for (i in 0 until 5) {
-            if (optimize.desire(Settings.Opt.Actions.cp)) {
-                val ccp = CondConstProp(cfg)
-                ccp.run()
-                ccp.postprocess()
+        var timeRemaining = 5000L
+        while (timeRemaining > 0) {
+            timeRemaining -= measureTimeMillis {
+                if (optimize.desire(Settings.Opt.Actions.cp)) {
+                    val ccp = CondConstProp(cfg)
+                    ccp.run()
+                    ccp.postprocess()
+                }
+
+                if (optimize.desire(Settings.Opt.Actions.dce)) {
+                    val dce = DeadCodeRem(cfg)
+                    dce.run()
+                    dce.postprocess()
+                }
+
+                if (optimize.desire(Settings.Opt.Actions.copy)) {
+                    val copypop = CopyProp(cfg)
+                    copypop.run()
+                    copypop.postprocess()
+                }
+
+                val newLIR = CFGDestroyer(cfg, lir).destroy()
+                newLIR.reorderBlocks()
+                cfg = CFGBuilder(newLIR).build()
             }
-
-            if (optimize.desire(Settings.Opt.Actions.dce)) {
-                val dce = DeadCodeRem(cfg)
-                dce.run()
-                dce.postprocess()
-            }
-
-            if (optimize.desire(Settings.Opt.Actions.copy)) {
-                val copypop = CopyProp(cfg)
-                copypop.run()
-                copypop.postprocess()
-            }
-
-            val newLIR = CFGDestroyer(cfg, lir).destroy()
-            newLIR.reorderBlocks()
-            cfg = CFGBuilder(newLIR).build()
+            count++
         }
 
         val postFile = outputCFG.getOutFile(lir.name, "final")
         postFile?.writeText(cfg.graphViz())
+        println("completed $count rounds for ${lir.name}")
     }
 
     fun destroy(): LIRFuncDecl {
