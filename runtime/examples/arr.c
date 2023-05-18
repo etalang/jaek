6dcdc974385d86d89b7f402dfdacfaf30a409c7b
Kate Meuse <katemeuse@gmail.com>
2023-05-17 21:52:35 -0400

    update runtime

--- runtime/examples/arr.c
+++
@@ -1,9 +0,0 @@
-#include "libeta.h"
-
-void ETA(main_paai)(etastring arg[]) {
-    etaint argc = etalength(arg);
-    etaint a;
-    for (a = 0; a < argc; a++) {
-        ETA(println_pai)(arg[a]);
-    }
-}
